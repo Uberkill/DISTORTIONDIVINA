6139ac19320e8805e5799eef3915083447de31b9
Uberkill <37767247+Uberkill@users.noreply.github.com>
2026-01-24 21:47:39 +0800

    lingeringgg

--- validate_json.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-try {
-    const content = fs.readFileSync('vercel.json', 'utf8');
-    JSON.parse(content);
-    console.log("VALID JSON");
-} catch (e) {
-    console.error("INVALID JSON:", e.message);
-}
